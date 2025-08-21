e8a1e30a03523baec250cb1a94394e119418721c
viva PHP <geroconde.gc@gmail.com>
2025-08-21 14:09:02 -0300

    DB Update

--- dbConection/db.php
+++ dbConection/db.php
@@ -1,23 +1,21 @@
 <?php
+$email = $_POST['Email'];
+$contraseña = $_POST['Contraseña'];
 
-$usuario = $_POST['Usuario'];
-$contaseña = $_POST ['Contraseña'];
+// conexión a la base de datos
+$conexion = mysqli_connect('localhost', 'root', '', 'dbvmng');
 
-//conexión a la base de datos
-$conexion = mysqli_connect('localhost','root','',"dbvmng");
-$consulta = "SELECT * FROM usuarios WHERE Usuario = '$usuario ' AND Contraseña = '$contaseña '";
-
+$consulta = "SELECT * FROM users WHERE Email='$email' AND Contraseña='$contraseña'";
 $resultado = mysqli_query($conexion, $consulta);
 
 $filas = mysqli_num_rows($resultado);
 
-if ($filas>0){
+if ($filas > 0) {
     header("Location: ../menu/index.html");
-
-}else{
+} else {
     echo "Error en la autenticación";
-
 }
 
 mysqli_free_result($resultado);
 mysqli_close($conexion);
+?>
