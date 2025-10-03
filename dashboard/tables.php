7d1434527c290e44f5097164c9f7c8ce9092da73
viva PHP <geroconde.gc@gmail.com>
2025-10-03 11:45:20 -0300

    move

--- dashboard/tables.php
+++ dashboard/tables.php
@@ -8,7 +8,7 @@
     <meta name="author" content="" />
     <title>Tablas - SB Admin</title>
     <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
-    <link href="css/styles.css" rel="stylesheet" />
+    <link href="../Estilos/stylesBootstrap.css" rel="stylesheet" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
 </head>
